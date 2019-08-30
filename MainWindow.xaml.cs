3e2bdacdaa7ae73ceb7230cdb7536f78ccfb22a3
Akideveloper <54722240+Akideveloper@users.noreply.github.com>
2019-08-30 21:29:52 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,17 +21,13 @@ namespace FIGHT_TEST_ANIMA
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {
-
-           
+        {    
 
         }
 
         private void FIGHT_BUTTON_Click(object sender, RoutedEventArgs e)
         {
-            bool Button_Click = true;
-            if (Button_Click == true)
-            {
+             {
                 int playerStrenght = 8;
                 int weaponPower = 8;
                 int playerHealth = 50;
@@ -40,38 +36,54 @@ namespace FIGHT_TEST_ANIMA
                 int Damage = 0;
                 Random randownValue = new Random();
                 int rV = randownValue.Next(1, 250);
+                int acVV = randownValue.Next(0, 1);
+                int accuracy = rV + 10; 
+                if (acVV == 1)
+                {
+                    rV = accuracy;
+                }
+                else
+                {
+                    accuracy = Damage;
+                }
+
                 this.RVSHOW.Text = Convert.ToString(rV);
                 if (rV < 51)
                 {
                     Damage = 0;
                     this.FIGHT_TEXT.Text = "DANEBEN EY!";
                 }
-                if (rV > 51 && rV < 100)
+                else  if (rV < 100)
 
                 {
                     Damage = 8;
-                    this.FIGHT_TEXT.Text = "Du machst 8 Punkte Schaden";
+                    this.FIGHT_TEXT.Text = "Du machst " + Damage + " Punkte Schaden!";
                 }
-                if (rV > 101 && rV < 150)
+                if (rV >= 101 && rV < 150)
                 {
                     Damage = 10;
-                    this.FIGHT_TEXT.Text = " Du machst 10 Punkte Schaden";
+                    this.FIGHT_TEXT.Text = "Du machst " + Damage + " Punkte Schaden!";
                 }
-                if (rV > 151 && rV < 200)
+                if (rV >= 151 && rV < 200)
                 {
                     Damage = 12;
-                    this.FIGHT_TEXT.Text = " Du machst 12 Punkte Schaden";
+                    this.FIGHT_TEXT.Text = "Du machst " + Damage + " Punkte Schaden!";
                 }
-                if (rV > 201 && rV < 225)
+                if (rV >= 201 && rV < 225)
                 {
                     Damage = 14;
-                    this.FIGHT_TEXT.Text = " Du machst 14 Punkte Schaden";
+                    this.FIGHT_TEXT.Text = "Du machst " + Damage + " Punkte Schaden!";
 
                 }
-                if (rV > 226 && rV < 250)
+                if (rV >= 226 && rV < 250)
+                {
+                     Damage = 16;
+                     this.FIGHT_TEXT.Text = "Du machst " + Damage + " Punkte Schaden!";
+                }
+                if (rV > 250)
                 {
                     Damage = 16;
-                   this.FIGHT_TEXT.Text =" Du machst 16 Punkte Schaden";
+                    this.FIGHT_TEXT.Text = "Du machst " + Damage + " Punkte Schaden! Das ist ein Volltreffer!";
                 }
             }
         }
